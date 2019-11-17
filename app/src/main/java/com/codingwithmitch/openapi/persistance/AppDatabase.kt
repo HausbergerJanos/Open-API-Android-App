7efd1298f480a355278ed26100fec6779cc81214
Janos Hausberger <hausberger.janos@codeyard.eu>
2019-11-17 18:02:42 +0100

    Room Persistence Setup

--- app/src/main/java/com/codingwithmitch/openapi/persistance/AppDatabase.kt
+++ app/src/main/java/com/codingwithmitch/openapi/persistance/AppDatabase.kt
@@ -1,5 +1,20 @@
 package com.codingwithmitch.openapi.persistance
 
-abstract class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.codingwithmitch.openapi.models.AccountProperties
+import com.codingwithmitch.openapi.models.AuthToken
+
+@Database(entities = [AuthToken::class, AccountProperties::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+
+    companion object {
+
+        const val DATABASE_NAME = "app_db"
+    }
+
+    abstract fun getAuthTokenDao(): AuthTokenDao
+
+    abstract fun getAccountPropertiesDao(): AccountPropertiesDao
 
 }
