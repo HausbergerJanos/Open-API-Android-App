6bacf4adbea519e2cf8e67c43f7b5a3211dcd840
Janos Hausberger <hausberger.janos@codeyard.eu>
2019-12-20 11:19:18 +0100

    RecyclerView Adapter, DiffUtil and ListUpdateCallback

--- app/src/main/java/com/codingwithmitch/openapi/ui/dashboard/blog/BlogFragment.kt
+++ app/src/main/java/com/codingwithmitch/openapi/ui/dashboard/blog/BlogFragment.kt
@@ -5,14 +5,26 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
 import com.codingwithmitch.openapi.R
+import com.codingwithmitch.openapi.models.BlogPost
+import com.codingwithmitch.openapi.ui.dashboard.blog.BlogRecyclerViewAdapter.Interaction
 import com.codingwithmitch.openapi.ui.dashboard.blog.state.BlogStateEvent
+import com.codingwithmitch.openapi.util.TopItemSpacingDecoration
 import kotlinx.android.synthetic.main.fragment_blog.*
+import javax.inject.Inject
 
-class BlogFragment : BaseBlogFragment(){
+class BlogFragment : BaseBlogFragment(), Interaction{
 
+    @Inject
+    lateinit var requestManager: RequestManager
+
+    private lateinit var adapter: BlogRecyclerViewAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,10 +37,13 @@ class BlogFragment : BaseBlogFragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        /**
         goViewBlogFragment?.setOnClickListener {
             findNavController().navigate(R.id.action_blogFragment_to_viewBlogFragment)
         }
+        */
 
+        initRecyclerView()
         subscribeObservers()
         executeSearch()
     }
@@ -55,8 +70,53 @@ class BlogFragment : BaseBlogFragment(){
             }
         })
 
-        viewModel.viewState.observe(viewLifecycleOwner, Observer { blogViewState ->
-            Log.d(TAG, "BlogFragment, ViewState: $blogViewState")
+        viewModel.viewState.observe(viewLifecycleOwner, Observer { viewState ->
+            viewState?.let { blogViewState ->
+                adapter.submitList(
+                    list = blogViewState.blogFields.blogList,
+                    isQueryExhausted = true
+                )
+            }
         })
+    }
+
+    private fun initRecyclerView() {
+        blog_post_recyclerview.apply {
+            layoutManager = LinearLayoutManager(this@BlogFragment.context)
+
+            val topItemSpacingDecoration = TopItemSpacingDecoration(30)
+            removeItemDecoration(topItemSpacingDecoration)
+            addItemDecoration(topItemSpacingDecoration)
+
+            this@BlogFragment.adapter = BlogRecyclerViewAdapter(
+                requestManager = requestManager,
+                interaction = this@BlogFragment
+            )
+
+            addOnScrollListener(object: RecyclerView.OnScrollListener() {
+
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                    val lastPosition = layoutManager.findLastVisibleItemPosition()
+                    if (lastPosition == this@BlogFragment.adapter.itemCount.minus(1)) {
+                        Toast.makeText(this@BlogFragment.context, "Load next page", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+
+            adapter = this@BlogFragment.adapter
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Clear references
+        blog_post_recyclerview?.adapter = null
+    }
+
+    override fun onItemSelected(position: Int, item: BlogPost) {
+        Toast.makeText(context, position.toString(), Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/codingwithmitch/openapi/util/GenericViewHolder.kt
@@ -0,0 +1,6 @@
+package com.codingwithmitch.openapi.util
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class GenericViewHolder (itemView: View): RecyclerView.ViewHolder(itemView)
